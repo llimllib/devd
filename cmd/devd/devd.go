4850a288ff234d8db3163c8a8da78bab08a37699
Bill Mill <bill@billmill.org>
2026-06-10 22:08:02 -0400

    replace kingpin with `flag`
    saves us an old, unmaintained, dependency

--- cmd/devd/devd.go
+++ cmd/devd/devd.go
@@ -1,194 +1,188 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/cortesi/termlog"
 	"github.com/llimllib/devd"
 	"github.com/mitchellh/go-homedir"
 	"github.com/toqueteos/webbrowser"
-	"gopkg.in/alecthomas/kingpin.v2"
 )
 
+// stringSlice implements flag.Value for flags that can be specified multiple times
+type stringSlice []string
+
+func (s *stringSlice) String() string { return strings.Join(*s, ", ") }
+func (s *stringSlice) Set(val string) error {
+	*s = append(*s, val)
+	return nil
+}
+
+func fatalf(format string, args ...any) {
+	fmt.Fprintf(os.Stderr, "devd: error: "+format+"\n", args...)
+	os.Exit(1)
+}
+
 func main() {
-	address := kingpin.Flag("address", "Address to listen on").
-		Short('A').
-		Default("127.0.0.1").
-		String()
+	var (
+		address      string
+		allIfaces    bool
+		certFile     string
+		forceColor   bool
+		downKbps     uint
+		logHeaders   bool
+		lrNaked      bool
+		lrRoutes     bool
+		moddMode     bool
+		latency      int
+		openBrowser  bool
+		port         int
+		credspec     string
+		quiet        bool
+		tlsFlag      bool
+		noTimestamps bool
+		logTime      bool
+		upKbps       uint
+		corsFlag     bool
+		debug        bool
+		version      bool
+		notfound     stringSlice
+		ignoreLogs   stringSlice
+		watch        stringSlice
+		excludes     stringSlice
+	)
 
-	allInterfaces := kingpin.Flag("all", "Listen on all addresses").
-		Short('a').
-		Bool()
+	flag.StringVar(&address, "address", "127.0.0.1", "Address to listen on")
+	flag.StringVar(&address, "A", "127.0.0.1", "Address to listen on (shorthand)")
 
-	certFile := kingpin.Flag("cert", "Certificate bundle file - enables TLS").
-		Short('c').
-		PlaceHolder("PATH").
-		ExistingFile()
+	flag.BoolVar(&allIfaces, "all", false, "Listen on all addresses")
+	flag.BoolVar(&allIfaces, "a", false, "Listen on all addresses (shorthand)")
 
-	forceColor := kingpin.Flag("color", "Enable colour output, even if devd is not connected to a terminal").
-		Short('C').
-		Bool()
+	flag.StringVar(&certFile, "cert", "", "Certificate bundle file - enables TLS")
+	flag.StringVar(&certFile, "c", "", "Certificate bundle file - enables TLS (shorthand)")
 
-	downKbps := kingpin.Flag(
-		"down",
-		"Throttle downstream from the client to N kilobytes per second",
-	).
-		PlaceHolder("N").
-		Short('d').
-		Default("0").
-		Uint()
+	flag.BoolVar(&forceColor, "color", false, "Enable colour output, even if devd is not connected to a terminal")
+	flag.BoolVar(&forceColor, "C", false, "Enable colour output (shorthand)")
 
-	notfound := kingpin.Flag("notfound", "Default when a static file is not found").
-		PlaceHolder("PATH").
-		Short('f').
-		Strings()
+	flag.UintVar(&downKbps, "down", 0, "Throttle downstream from the client to N kilobytes per second")
+	flag.UintVar(&downKbps, "d", 0, "Throttle downstream (shorthand)")
 
-	logHeaders := kingpin.Flag("logheaders", "Log headers").
-		Short('H').
-		Default("false").
-		Bool()
+	flag.Var(&notfound, "notfound", "Default when a static file is not found (can be repeated)")
+	flag.Var(&notfound, "f", "Default when a static file is not found (shorthand, can be repeated)")
 
-	ignoreLogs := kingpin.Flag(
-		"ignore",
-		"Disable logging matching requests. Regexes are matched over 'host/path'",
-	).
-		Short('I').
-		PlaceHolder("REGEX").
-		Strings()
+	flag.BoolVar(&logHeaders, "logheaders", false, "Log headers")
+	flag.BoolVar(&logHeaders, "H", false, "Log headers (shorthand)")
 
-	livereloadNaked := kingpin.Flag("livereload", "Enable livereload").
-		Short('L').
-		Default("false").
-		Bool()
+	flag.Var(&ignoreLogs, "ignore", "Disable logging matching requests. Regexes are matched over 'host/path' (can be repeated)")
+	flag.Var(&ignoreLogs, "I", "Disable logging matching requests (shorthand, can be repeated)")
 
-	livereloadRoutes := kingpin.Flag("livewatch", "Enable livereload and watch for static file changes").
-		Short('l').
-		Default("false").
-		Bool()
+	flag.BoolVar(&lrNaked, "livereload", false, "Enable livereload")
+	flag.BoolVar(&lrNaked, "L", false, "Enable livereload (shorthand)")
 
-	moddMode := kingpin.Flag("modd", "Modd is our parent - synonym for -LCt").
-		Short('m').
-		Bool()
+	flag.BoolVar(&lrRoutes, "livewatch", false, "Enable livereload and watch for static file changes")
+	flag.BoolVar(&lrRoutes, "l", false, "Enable livereload and watch (shorthand)")
 
-	latency := kingpin.Flag("latency", "Add N milliseconds of round-trip latency").
-		PlaceHolder("N").
-		Short('n').
-		Default("0").
-		Int()
+	flag.BoolVar(&moddMode, "modd", false, "Modd is our parent - synonym for -LCt")
+	flag.BoolVar(&moddMode, "m", false, "Modd is our parent (shorthand)")
 
-	openBrowser := kingpin.Flag("open", "Open browser window on startup").
-		Short('o').
-		Default("false").
-		Bool()
+	flag.IntVar(&latency, "latency", 0, "Add N milliseconds of round-trip latency")
+	flag.IntVar(&latency, "n", 0, "Add N milliseconds of round-trip latency (shorthand)")
 
-	port := kingpin.Flag(
-		"port",
-		"Port to listen on - if not specified, devd will auto-pick a sensible port",
-	).
-		Short('p').
-		Int()
+	flag.BoolVar(&openBrowser, "open", false, "Open browser window on startup")
+	flag.BoolVar(&openBrowser, "o", false, "Open browser window on startup (shorthand)")
 
-	credspec := kingpin.Flag(
-		"password",
-		"HTTP basic password protection",
-	).
-		PlaceHolder("USER:PASS").
-		Short('P').
-		String()
+	flag.IntVar(&port, "port", 0, "Port to listen on - if not specified, devd will auto-pick a sensible port")
+	flag.IntVar(&port, "p", 0, "Port to listen on (shorthand)")
 
-	quiet := kingpin.Flag("quiet", "Silence all logs").
-		Short('q').
-		Default("false").
-		Bool()
+	flag.StringVar(&credspec, "password", "", "HTTP basic password protection (USER:PASS)")
+	flag.StringVar(&credspec, "P", "", "HTTP basic password protection (shorthand)")
 
-	tls := kingpin.Flag("tls", "Serve TLS with auto-generated self-signed certificate (~/.devd.cert)").
-		Short('s').
-		Default("false").
-		Bool()
+	flag.BoolVar(&quiet, "quiet", false, "Silence all logs")
+	flag.BoolVar(&quiet, "q", false, "Silence all logs (shorthand)")
 
-	noTimestamps := kingpin.Flag("notimestamps", "Disable timestamps in output").
-		Short('t').
-		Default("false").
-		Bool()
+	flag.BoolVar(&tlsFlag, "tls", false, "Serve TLS with auto-generated self-signed certificate (~/.devd.cert)")
+	flag.BoolVar(&tlsFlag, "s", false, "Serve TLS (shorthand)")
 
-	logTime := kingpin.Flag("logtime", "Log timing").
-		Short('T').
-		Default("false").
-		Bool()
+	flag.BoolVar(&noTimestamps, "notimestamps", false, "Disable timestamps in output")
+	flag.BoolVar(&noTimestamps, "t", false, "Disable timestamps in output (shorthand)")
 
-	upKbps := kingpin.Flag(
-		"up",
-		"Throttle upstream from the client to N kilobytes per second",
-	).
-		PlaceHolder("N").
-		Short('u').
-		Default("0").
-		Uint()
+	flag.BoolVar(&logTime, "logtime", false, "Log timing")
+	flag.BoolVar(&logTime, "T", false, "Log timing (shorthand)")
 
-	watch := kingpin.Flag("watch", "Watch path to trigger livereload").
-		PlaceHolder("PATH").
-		Short('w').
-		Strings()
+	flag.UintVar(&upKbps, "up", 0, "Throttle upstream from the client to N kilobytes per second")
+	flag.UintVar(&upKbps, "u", 0, "Throttle upstream (shorthand)")
 
-	cors := kingpin.Flag("crossdomain", "Set the CORS headers to allow everything (origin, credentials, headers, methods)").
-		Short('X').
-		Default("false").
-		Bool()
+	flag.Var(&watch, "watch", "Watch path to trigger livereload (can be repeated)")
+	flag.Var(&watch, "w", "Watch path to trigger livereload (shorthand, can be repeated)")
 
-	excludes := kingpin.Flag("exclude", "Glob pattern for files to exclude from livereload").
-		PlaceHolder("PATTERN").
-		Short('x').
-		Strings()
+	flag.BoolVar(&corsFlag, "crossdomain", false, "Set the CORS headers to allow everything (origin, credentials, headers, methods)")
+	flag.BoolVar(&corsFlag, "X", false, "Set the CORS headers (shorthand)")
 
-	debug := kingpin.Flag("debug", "Debugging for devd development").
-		Default("false").
-		Bool()
+	flag.Var(&excludes, "exclude", "Glob pattern for files to exclude from livereload (can be repeated)")
+	flag.Var(&excludes, "x", "Glob pattern for files to exclude (shorthand, can be repeated)")
 
-	routes := kingpin.Arg(
-		"route",
-		`Routes have the following forms:
-			[SUBDOMAIN]/<PATH>=<DIR>
-			[SUBDOMAIN]/<PATH>=<URL>
-			<DIR>
-			<URL>
-		`,
-	).Required().Strings()
+	flag.BoolVar(&debug, "debug", false, "Debugging for devd development")
 
-	kingpin.CommandLine.HelpFlag.Short('h')
-	kingpin.Version(devd.Version)
+	flag.BoolVar(&version, "version", false, "Print version and exit")
+	flag.BoolVar(&version, "v", false, "Print version and exit (shorthand)")
 
-	kingpin.Parse()
-
-	if *moddMode {
-		*forceColor = true
-		*noTimestamps = true
-		*livereloadNaked = true
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: devd [flags] route [route ...]\n\n")
+		fmt.Fprintf(os.Stderr, "Routes have the following forms:\n")
+		fmt.Fprintf(os.Stderr, "    [SUBDOMAIN]/<PATH>=<DIR>\n")
+		fmt.Fprintf(os.Stderr, "    [SUBDOMAIN]/<PATH>=<URL>\n")
+		fmt.Fprintf(os.Stderr, "    <DIR>\n")
+		fmt.Fprintf(os.Stderr, "    <URL>\n\n")
+		fmt.Fprintf(os.Stderr, "Flags:\n")
+		flag.PrintDefaults()
 	}
 
-	realAddr := *address
-	if *allInterfaces {
+	flag.Parse()
+
+	if version {
+		fmt.Printf("devd %s\n", devd.Version)
+		os.Exit(0)
+	}
+
+	routes := flag.Args()
+	if len(routes) == 0 {
+		fmt.Fprintf(os.Stderr, "devd: error: required argument 'route' not provided\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if moddMode {
+		forceColor = true
+		noTimestamps = true
+		lrNaked = true
+	}
+
+	realAddr := address
+	if allIfaces {
 		realAddr = "0.0.0.0"
 	}
 
 	var creds *devd.Credentials
-	if *credspec != "" {
+	if credspec != "" {
 		var err error
-		creds, err = devd.CredentialsFromSpec(*credspec)
+		creds, err = devd.CredentialsFromSpec(credspec)
 		if err != nil {
-			kingpin.Fatalf("%s", err)
-			return
+			fatalf("%s", err)
 		}
 	}
 
 	hdrs := make(http.Header)
-	if *cors {
+	if corsFlag {
 		hdrs.Set("Access-Control-Allow-Credentials", "true")
 	}
 
 	var servingScheme string
-	if *tls {
+	if tlsFlag {
 		servingScheme = "https"
 	} else {
 		servingScheme = "http"
@@ -196,49 +190,49 @@ func main() {
 
 	dd := devd.Devd{
 		// Shaping
-		Latency:       *latency,
-		DownKbps:      *downKbps,
-		UpKbps:        *upKbps,
+		Latency:       latency,
+		DownKbps:      downKbps,
+		UpKbps:        upKbps,
 		ServingScheme: servingScheme,
 
 		AddHeaders: &hdrs,
 
 		// Livereload
-		LivereloadRoutes: *livereloadRoutes,
-		Livereload:       *livereloadNaked,
-		WatchPaths:       *watch,
-		Excludes:         *excludes,
+		LivereloadRoutes: lrRoutes,
+		Livereload:       lrNaked,
+		WatchPaths:       watch,
+		Excludes:         excludes,
 
-		Cors: *cors,
+		Cors: corsFlag,
 
 		Credentials: creds,
 	}
 
-	if err := dd.AddRoutes(*routes, *notfound); err != nil {
-		kingpin.Fatalf("%s", err)
+	if err := dd.AddRoutes(routes, notfound); err != nil {
+		fatalf("%s", err)
 	}
 
-	if err := dd.AddIgnores(*ignoreLogs); err != nil {
-		kingpin.Fatalf("%s", err)
+	if err := dd.AddIgnores(ignoreLogs); err != nil {
+		fatalf("%s", err)
 	}
 
 	logger := termlog.NewLog()
-	if *quiet {
+	if quiet {
 		logger.Quiet()
 	}
-	if *debug {
+	if debug {
 		logger.Enable("debug")
 	}
-	if *logTime {
+	if logTime {
 		logger.Enable("timer")
 	}
-	if *logHeaders {
+	if logHeaders {
 		logger.Enable("headers")
 	}
-	if *forceColor {
+	if forceColor {
 		logger.Color(true)
 	}
-	if *noTimestamps {
+	if noTimestamps {
 		logger.TimeFmt = ""
 	}
 
@@ -246,36 +240,36 @@ func main() {
 		logger.Say("Route %s -> %s", i.MuxMatch(), i.Endpoint.String())
 	}
 
-	if *tls {
+	if tlsFlag {
 		home, err := homedir.Dir()
 		if err != nil {
-			kingpin.Fatalf("Could not get user's homedir: %s", err)
+			fatalf("Could not get user's homedir: %s", err)
 		}
 		dst := path.Join(home, ".devd.cert")
 		if _, err := os.Stat(dst); os.IsNotExist(err) {
 			err := devd.GenerateCert(dst)
 			if err != nil {
-				kingpin.Fatalf("Could not generate cert: %s", err)
+				fatalf("Could not generate cert: %s", err)
 			}
 		}
-		*certFile = dst
+		certFile = dst
 	}
 
 	err := dd.Serve(
 		realAddr,
-		*port,
-		*certFile,
+		port,
+		certFile,
 		logger,
 		func(url string) {
-			if *openBrowser {
+			if openBrowser {
 				err := webbrowser.Open(url)
 				if err != nil {
-					kingpin.Fatalf("Failed to open browser: %s", err)
+					fatalf("Failed to open browser: %s", err)
 				}
 			}
 		},
 	)
 	if err != nil {
-		kingpin.Fatalf("%s", err)
+		fatalf("%s", err)
 	}
 }
